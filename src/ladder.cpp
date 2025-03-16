bd42ac193b10e6145f638d7a2faec607dbf58ad7
William Saleh <william.saleh578@gmail.com>
2025-03-16 00:35:42 +0000

    s

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -83,7 +83,7 @@ void print_word_ladder(const vector<string>& ladder) {
             cout << ladder[i];
             if (i < ladder.size() - 1) cout << " ";
         }
-        cout << endl;
+        cout << " \n";
     }
 }
 
